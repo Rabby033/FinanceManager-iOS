643f3a5bda0c73b4c2ef4153e5d5a7762a1afbee
rabby033 <rabby11011@gmail.com>
2024-09-18 02:07:08 +0600

    Basic structure of onboarding pages are implemented

---
+++ FinanceManager/FinanceManager/FinanceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagerApp.swift
+//  FinanceManager
+//
+//  Created by Mahedi Hassan Rabby on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FinanceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingScreen()
+        }
+    }
+}
---
+++ FinanceManager/FinanceManager/PreferenceRepository/PreferenceRepository.swift
@@ -0,0 +1,8 @@
+//
+//  PreferenceRepository.swift
+//  FinanceManager
+//
+//  Created by Mahedi Hassan Rabby on 17/9/24.
+//
+
+import Foundation
